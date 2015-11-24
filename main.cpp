dd05c4bf77304c5aacd5d8411d47db0d0e86305d
Lidija Šikić <ligalic@vsite.hr>
2015-11-24 02:15:01 +0100

    skoro sve

--- main.cpp
+++ main.cpp
@@ -2,31 +2,93 @@
 #include <cmath>
 #include "rc.h"
 
-int NumDialog::IDD(){
+COLORREF back;
+
+COLORREF GetColor(HWND parent, COLORREF col)
+{
+	
+	COLORREF custCols[16] = { 0 };
+	CHOOSECOLOR cc;
+	ZeroMemory(&cc, sizeof cc);
+	cc.lStructSize = sizeof cc;
+	cc.Flags = CC_FULLOPEN | CC_RGBINIT;
+	cc.hwndOwner = parent;
+	cc.lpCustColors = custCols;
+	cc.rgbResult = col;
+	if (ChooseColor(&cc))
+		col = cc.rgbResult;
+	return col;
+	
+}
+int GetNumber(HWND parent, int br)
+{
+	
+	NumDialog numDial;
+	if (numDial.DoModal(NULL, parent) == IDOK)
+	{
+		br = numDial.br;		
+	}
+	return br;
+}
+
+int NumDialog::IDD()
+{
 	return IDD_NUMBER; 
 }
 
-bool NumDialog::OnInitDialog(){
+bool NumDialog::OnInitDialog()
+{
+	
+	SetInt(IDC_EDIT1,br);
 	return true;
 }
 
-bool NumDialog::OnOK(){
-	return true;
+bool NumDialog::OnOK()
+{
+	try 
+	{
+		NumDialog::br = GetInt(IDC_EDIT1);
+		return true;
+	}
+	catch (XCtrl )
+	{
+		MessageBox(*this, "Krivi unos", "Greška", MB_OK | MB_ICONERROR);
+		return false;
+	}
 }
 
-void MainWindow::OnPaint(HDC hdc){
+void MainWindow::OnPaint(HDC hdc)
+{
+	double y, x;
+	
+	int r = 100;
+	HBRUSH brush = CreateSolidBrush(back);
+	SetROP2(hdc, R2_XORPEN);
+
+	for (int k = 0; k < br; k++)
+	{
+		y = (int)(-r*(sin(k*(2 * atan(1) * 4 / br)))); //atan(1)*4  = pi
+		x = (int)(-r*(cos(k*(2 * atan(1) * 4 / br))));
+		
+		Ellipse(hdc, x - r + 400, y - r + 250, x + r + 400, y + r + 250);
+	}
+
+	DeleteObject(brush);
 }
 
 void MainWindow::OnCommand(int id){
 	switch(id){
 		case ID_COLOR: 
+			back = GetColor(*this, back);			
 			break;
 		case ID_NUMBER: 
+			br = GetNumber(*this, br);
 			break;
 		case ID_EXIT: 
 			DestroyWindow(*this); 
 			break;
 	}
+	InvalidateRect(*this, NULL, true);
 }
 
 void MainWindow::OnDestroy(){
--- main.h
+++ main.h
@@ -2,13 +2,22 @@
 #include "nwpdlg.h"
 
 class NumDialog : public Dialog {
+public:
+	int br;
+	NumDialog() : br(0) {}
 protected:
 	int IDD();
 	bool OnInitDialog();
 	bool OnOK();
 };
 
+
 class MainWindow : public Window {
+
+public:
+	COLORREF col;
+	int br;
+	
 protected:
 	void OnPaint(HDC hdc);
 	void OnCommand(int id);
