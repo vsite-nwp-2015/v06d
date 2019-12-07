7784668ebe1dd4cac8edb2d633d827d15aa48370
ivek81cro <ivek81cro@gmail.com>
2019-12-07 12:14:20 +0100

    commit

--- main.cpp
+++ main.cpp
@@ -7,25 +7,72 @@ int NumDialog::IDD(){
 }
 
 bool NumDialog::OnInitDialog(){
+	SetInt(IDC_EDIT1, num);
 	return true;
 }
 
 bool NumDialog::OnOK(){
+	try {
+		num = GetInt(IDC_EDIT1);
+		if (num <= 0 || num > 10000)
+			return false;
+	}
+	catch (XCtrl&) {
+		return false;
+	}
 	return true;
 }
 
+void MainWindow::NewColor() {
+	COLORREF custCols[16] = { 0 };
+	CHOOSECOLOR cc;
+	ZeroMemory(&cc, sizeof cc);
+	cc.lStructSize = sizeof cc;
+	cc.Flags = CC_FULLOPEN | CC_RGBINIT;
+	cc.lpCustColors = custCols;
+	cc.rgbResult = selColor;
+	if (ChooseColor(&cc) == IDOK) {
+		selColor = cc.rgbResult;
+		InvalidateRect(*this, NULL, true);
+	}
+}
+void MainWindow::NewNumber() {
+	NumDialog nd(numCircle);
+
+	if (nd.DoModal(0, *this) == IDOK) {
+		numCircle = nd.getNum();
+		InvalidateRect(*this, NULL, true);
+	}
+}
+
 void MainWindow::OnPaint(HDC hdc){
+	RECT rect;
+	GetClientRect(*this, &rect);
+	int r = rect.bottom / 4;
+	HBRUSH brush = CreateSolidBrush(selColor);
+	HGDIOBJ holdBrush = SelectObject(hdc, brush);
+	SelectPen(hdc, GetStockObject(NULL_PEN));
+	SetROP2(hdc, R2_NOTXORPEN);
+	for (int i = 0; i < numCircle; ++i) {
+		double a = 2 * 3.14 * i / numCircle;
+		double x = rect.right / 2 + r * cos(a);
+		double y = rect.bottom / 2 + r * sin(a);
+		Ellipse(hdc, x + r, y + r, x - r, y - r);
+	}
+	DeleteObject(SelectObject(hdc, holdBrush));
 }
 
 void MainWindow::OnCommand(int id){
 	switch(id){
-		case ID_COLOR: 
-			break;
-		case ID_NUMBER: 
-			break;
-		case ID_EXIT: 
-			DestroyWindow(*this); 
-			break;
+	case ID_COLOR:
+		NewColor();
+		break;
+	case ID_NUMBER:
+		NewNumber();
+		break;
+	case ID_EXIT:
+		DestroyWindow(*this);
+		break;
 	}
 }
 
@@ -37,7 +84,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	Application app;
 	MainWindow wnd;	
-	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP", 
+	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 6d", 
 		(int)LoadMenu(hInstance, MAKEINTRESOURCE(IDM_MAIN)));	
 	return app.Run();
 }
--- main.h
+++ main.h
@@ -2,6 +2,10 @@
 #include "nwpdlg.h"
 
 class NumDialog : public Dialog {
+	int num;
+public:
+	NumDialog(int n) : num(n) {}
+	int getNum() { return num; };
 protected:
 	int IDD();
 	bool OnInitDialog();
@@ -9,8 +13,12 @@ protected:
 };
 
 class MainWindow : public Window {
+	COLORREF selColor = RGB(0, 0, 0);
+	int numCircle = 5;
 protected:
 	void OnPaint(HDC hdc);
 	void OnCommand(int id);
 	void OnDestroy();
+	void NewColor();
+	void NewNumber();
 };
