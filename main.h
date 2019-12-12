6ac011227dce433209ac4032fc45853baff6cc25
Senad Mandžić <semandzi@vsite.hr>
2019-12-12 17:01:24 +0100

    Deklarirao i definirao klasu

--- main.h
+++ main.h
@@ -2,6 +2,12 @@
 #include "nwpdlg.h"
 
 class NumDialog : public Dialog {
+	int number;
+public:
+	NumDialog(int n) : number(n) {}
+	int getNumber() { return number; }
+
+
 protected:
 	int IDD();
 	bool OnInitDialog();
@@ -10,7 +16,11 @@ protected:
 
 class MainWindow : public Window {
 protected:
+	COLORREF selColor = RGB(0, 0, 0);
+	int numCircle = 5;
 	void OnPaint(HDC hdc);
 	void OnCommand(int id);
 	void OnDestroy();
+	void NewColor();
+	void NewNumber();
 };
