4b630b9f1c9ca2260e96b29b41026e49d95d69d6
esvegot <esvegot@HR-00004162>
2020-01-20 16:30:08 +0100

    Finish Lab06D

--- main.cpp
+++ main.cpp
@@ -1,44 +1,89 @@
+#define _USE_MATH_DEFINES
 #include "main.h"
 #include <cmath>
 #include "rc.h"
-
-int NumDialog::IDD(){
-	return IDD_NUMBER; 
+int cCalc(HWND parent, int scn) {
+	NumDialog d;
+	d.i = scn;
+	if (IDOK != d.DoModal(NULL, parent))
+	{
+		return scn;
+	}
+	scn = d.i;
 }
-
-bool NumDialog::OnInitDialog(){
+bool NumDialog::OnInitDialog() {
+	SetInt(IDC_EDIT1, i);
 	return true;
 }
-
-bool NumDialog::OnOK(){
-	return true;
+int NumDialog::IDD() {
+	return IDD_NUMBER;
 }
 
-void MainWindow::OnPaint(HDC hdc){
+COLORREF ClCh(HWND parent, COLORREF cursor) {
+	COLORREF c[16] = { 0 };
+	CHOOSECOLOR clr;
+	ZeroMemory(&c, sizeof clr);
+	clr.lStructSize = sizeof clr;
+	clr.Flags = CC_FULLOPEN | CC_RGBINIT;
+	clr.hwndOwner = parent;
+	clr.lpCustColors = c;
+	clr.rgbResult = cursor;
+	if (!ChooseColor(&clr)) {
+		return cursor;
+	}
+	cursor = clr.rgbResult;
 }
 
-void MainWindow::OnCommand(int id){
-	switch(id){
-		case ID_COLOR: 
-			break;
-		case ID_NUMBER: 
-			break;
-		case ID_EXIT: 
-			DestroyWindow(*this); 
-			break;
+void MainWindow::OnPaint(HDC hdc) {
+	RECT rt;
+	HBRUSH brh = CreateSolidBrush(cl);
+	HGDIOBJ hlr = SelectObject(hdc, brh);
+	GetClientRect(*this, &rt);
+	SelectObject(hdc, GetStockObject(NULL_PEN));
+	SetROP2(hdc, R2_NOTXORPEN);
+	for (int i = 0; i < scn; ++i) {
+		Ellipse(hdc, (rt.right / 2 + (rt.bottom / 4) * cos((double)(2 * M_PI * i / scn))) - (rt.bottom / 4), (rt.bottom / 2 + (rt.bottom / 4) * sin((double)(2 * M_PI * i / scn))) - (rt.bottom / 4), (rt.right / 2 + (rt.bottom / 4) * cos((double)(2 * M_PI * i / scn))) + (rt.bottom / 4), (rt.bottom / 2 + (rt.bottom / 4) * sin((double)(2 * M_PI * i / scn))) + (rt.bottom / 4));
+	}
+
+	DeleteObject(SelectObject(hdc, hlr));
+}
+bool NumDialog::OnOK() {
+	try {
+		i = GetInt(IDC_EDIT1);
+		return true;
+	}
+	catch (XCtrl)
+	{
+		return false;
 	}
 }
 
-void MainWindow::OnDestroy(){
+void MainWindow::OnCommand(int id) {
+	switch (id) {
+	case ID_EXIT:
+		DestroyWindow(*this);
+		break;
+	case ID_COLOR:
+		cl = ClCh(*this, cl);
+		InvalidateRect(*this, NULL, true);
+		break;
+	case ID_NUMBER:
+		scn = cCalc(*this, scn);
+		InvalidateRect(*this, NULL, true);
+		break;
+	}
+	InvalidateRect(*this, NULL, TRUE);
+}
+
+void MainWindow::OnDestroy() {
 	::PostQuitMessage(0);
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	Application app;
-	MainWindow wnd;	
-	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP", 
-		(int)LoadMenu(hInstance, MAKEINTRESOURCE(IDM_MAIN)));	
+	MainWindow wnd;
+	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP",
+		(int)LoadMenu(hInstance, MAKEINTRESOURCE(IDM_MAIN)));
 	return app.Run();
 }
-
--- main.h
+++ main.h
@@ -1,16 +1,20 @@
 #include "nwpwin.h"
 #include "nwpdlg.h"
-
-class NumDialog : public Dialog {
-protected:
-	int IDD();
-	bool OnInitDialog();
-	bool OnOK();
-};
-
 class MainWindow : public Window {
+public:
+	COLORREF cl = RGB(150, 250, 150);
+	int scn = 12;
 protected:
 	void OnPaint(HDC hdc);
 	void OnCommand(int id);
 	void OnDestroy();
+};
+class NumDialog : public Dialog {
+public:
+	int i = 1;
+
+protected:
+	int IDD();
+	bool OnInitDialog();
+	bool OnOK();
 };
