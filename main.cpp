057cdedf2a67ae6b7d413d2db6a9d2673b30a0be
Ante Žunić <anzunic@vsite.hr>
2021-04-19 20:29:23 +0200

    Rjesenje vjezbe 6d

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include "main.h"
 #include <cmath>
 #include "rc.h"
+#include <windows.h>
+
+class sel_obj {
+	HDC hdc;
+	HGDIOBJ hOld;
+public:
+	sel_obj(HDC hdc, HGDIOBJ hObj) :
+		hdc(hdc), hOld(SelectObject(hdc, hObj)) { }
+	~sel_obj() { SelectObject(hdc, hOld); }
+};
 
 int number_dialog::idd() const {
 	return IDD_NUMBER;
@@ -9,17 +19,61 @@ bool number_dialog::on_init_dialog() {
 	return true;
 }
 bool number_dialog::on_ok() {
+	BOOL asd = true;
+	try {
+		this->glavni->BrojKrugova = dialog::get_int(IDC_EDIT1);
+		InvalidateRect(*(this->glavni), NULL, true);
+	}
+	catch (std::runtime_error) {
+		MessageBox(*this, _T("Unos mora biti broj"), 0, MB_OK);
+		return false;
+	}
 	return true;
 }
 
+
 void main_window::on_paint(HDC hdc) {
+	POINT sredina;
+	RECT rect;
+	GetClientRect(*this, &rect);
+	sredina.x = rect.right / 2;
+	sredina.y = rect.bottom / 2;
+	int radijus = 100;
+	HBRUSH h = CreateSolidBrush(this->color);
+	//sel_obj(hdc, h);
+	SelectObject(hdc, h);
+	SetROP2(hdc, R2_NOTXORPEN);
+	for (int i = 0; i < this->BrojKrugova; ++i) {
+		double a = this->pi * 2 * i / BrojKrugova;
+		int centerX = sredina.x + radijus * sin(a);
+		int centerY = sredina.y + radijus * cos(a);
+		Ellipse(hdc, centerX - radijus, centerY - radijus, centerX + radijus, centerY + radijus);
+	}
+	DeleteObject(SelectObject(hdc, h));
+	
 }
 
 void main_window::on_command(int id){
+	number_dialog nd;
+	nd.glavni = this;
 	switch(id){
-		case ID_COLOR: 
+		case ID_COLOR: {
+			CHOOSECOLOR ColorDialog;
+			COLORREF acrCustClr[16] = { 0 };
+			ZeroMemory(&ColorDialog, sizeof ColorDialog);
+			ColorDialog.lStructSize = sizeof ColorDialog;
+			ColorDialog.hwndOwner = *this;
+			ColorDialog.lpCustColors = acrCustClr;
+			ColorDialog.rgbResult = this->color;
+			ColorDialog.Flags = CC_FULLOPEN | CC_RGBINIT;
+			if (ChooseColor(&ColorDialog) == TRUE) {
+				this->color = ColorDialog.rgbResult;
+			}
+			InvalidateRect(*this, NULL, true);
 			break;
-		case ID_NUMBER: 
+		}
+		case ID_NUMBER:
+			nd.do_modal(0, *this);
 			break;
 		case ID_EXIT: 
 			DestroyWindow(*this); 
--- main.h
+++ main.h
@@ -1,16 +1,24 @@
 #include "nwpwin.h"
 #include "nwpdlg.h"
 
-class number_dialog : public vsite::nwp::dialog {
-protected:
-	int idd() const override;
-	bool on_init_dialog() override;
-	bool on_ok() override;
-};
+
 
 class main_window : public vsite::nwp::window {
+public:
+	const double pi = 3.14159265359;
+	int BrojKrugova = 8;
+	COLORREF color = RGB(255, 255, 255);
 protected:
 	void on_paint(HDC hdc) override;
 	void on_command(int id) override;
 	void on_destroy() override;
+};
+
+class number_dialog : public vsite::nwp::dialog {
+public:
+	main_window* glavni = NULL;
+protected:
+	int idd() const override;
+	bool on_init_dialog() override;
+	bool on_ok() override;
 };
