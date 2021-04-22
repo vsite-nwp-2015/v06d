398adf964f267ad3816e8ce7f7be923932b3f905
Jasmin Husić <jhsc1988@gmail.com>
2021-04-22 09:53:13 +0200

    v6 done

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#define _USE_MATH_DEFINES
+
 #include "main.h"
 #include <cmath>
 #include "rc.h"
@@ -6,25 +8,78 @@ int number_dialog::idd() const {
 	return IDD_NUMBER;
 }
 bool number_dialog::on_init_dialog() {
+	set_int(IDC_EDIT1,broj);
 	return true;
 }
 bool number_dialog::on_ok() {
+	broj = dialog::get_int(IDC_EDIT1);
 	return true;
 }
 
 void main_window::on_paint(HDC hdc) {
+
+	RECT rect;
+	GetClientRect(*this, &rect);
+	HBRUSH brush = CreateSolidBrush(set_color);
+	HGDIOBJ holdBrush = SelectObject(hdc, brush);
+
+	SelectPen(hdc, GetStockObject(NULL_PEN));
+
+	// centering circle
+	// specifies which device point maps to the window origin. default (0, 0).
+	SetViewportOrgEx(hdc, rect.right / 2 , rect.bottom / 2, NULL);
+	SetROP2(hdc, R2_NOTXORPEN);
+	
+	int radius = rect.bottom / 4;
+	
+	// https://math.stackexchange.com/a/206662
+	for (int n = 0; n < k; ++n) {
+		double a = 2 * n * M_PI / k;
+		double x = radius * cos(a);
+		double y = radius * sin(a);
+		
+		// left top right bottom
+		::Ellipse(hdc, x + radius, y + radius, x - radius, y - radius);
+	}
+	DeleteObject(SelectObject(hdc, holdBrush));
+}
+
+void main_window::new_color() {
+
+	COLORREF custCols[16] = { 0 };
+	CHOOSECOLOR cc;
+	ZeroMemory(&cc, sizeof cc);
+	cc.lStructSize = sizeof cc;
+	cc.Flags = CC_FULLOPEN | CC_RGBINIT;
+	cc.hwndOwner = *this;
+	cc.lpCustColors = custCols;
+	cc.rgbResult = set_color;
+	if (ChooseColor(&cc))
+		set_color = cc.rgbResult;
+}
+
+int main_window::new_number() {
+
+	number_dialog nd(k);
+		if (nd.do_modal(0, *this) == IDOK) {
+			k = nd.get_int();
+		return k;
+	}
 }
 
 void main_window::on_command(int id){
 	switch(id){
 		case ID_COLOR: 
+			new_color();	
 			break;
 		case ID_NUMBER: 
+			new_number();
 			break;
 		case ID_EXIT: 
 			DestroyWindow(*this); 
 			break;
 	}
+	InvalidateRect(*this, NULL, TRUE);
 }
 
 void main_window::on_destroy(){
--- main.h
+++ main.h
@@ -2,6 +2,13 @@
 #include "nwpdlg.h"
 
 class number_dialog : public vsite::nwp::dialog {
+private:
+	int broj = 0;
+
+public:
+	number_dialog(int n) : broj(n) {};
+	int get_int() { return broj; };
+
 protected:
 	int idd() const override;
 	bool on_init_dialog() override;
@@ -9,8 +16,13 @@ protected:
 };
 
 class main_window : public vsite::nwp::window {
+	int k = 3; // broj krugova (default)
+	COLORREF set_color = RGB(255, 128, 0);
 protected:
 	void on_paint(HDC hdc) override;
 	void on_command(int id) override;
 	void on_destroy() override;
+
+	int new_number();
+	void new_color();
 };
